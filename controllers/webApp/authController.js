744fd65062564e1f5663f5d9e7ced3e5c7ef1ee0
omarhashy <omarmagdyhashy@gmail.com>
2024-11-08 16:35:21 +0200

    updating ui

--- controllers/webApp/authController.js
+++ controllers/webApp/authController.js
@@ -5,3 +5,28 @@ exports.register = (req, res, next) => {
   };
   res.render('auth/auth.ejs',context);
 };
+
+
+exports.login = (req, res, next) => {
+  context = {
+    pageTile: "Login",
+    PageHeader: "Login",
+  };
+  res.render("auth/auth.ejs", context);
+};
+exports.resetPassword = (req, res, next) => {
+  context = {
+    pageTile: "Reset Password",
+    PageHeader: "Reset Password",
+  };
+  res.render("auth/auth.ejs", context);
+};
+
+
+exports.resetPasswordToken = (req, res, next) => {
+  context = {
+    pageTile: "Create a new password",
+    PageHeader: "Create a new password",
+  };
+  res.render("auth/auth.ejs", context);
+};
--- routes/webApp/authRoutes.js
+++ routes/webApp/authRoutes.js
@@ -3,10 +3,10 @@ const authController = require("../../controllers/webApp/authController");
 const router = express.Router();
 
 router.get("/register", authController.register);
+router.get("/login", authController.login);
+router.get("/reset-password", authController.resetPassword);
+router.get("/reset-password/:token",authController.resetPasswordToken);
 
-router.get("/login");
-router.get("/reset-password");
-router.get("/reset-password/:token");
 router.get("/verify/:token");
 
 module.exports = router;
